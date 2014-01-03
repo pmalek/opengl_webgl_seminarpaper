c2547c3bcac32902504ba2a4171a84f0a4123cb7
Patryk Malek <malekpatryk@gmail.com>
2014-01-03 17:52:30 +0100

    Exemplar code in example1.java, change codestyle.tex, and main.tex

---
+++ example1.java
@@ -0,0 +1,18 @@
+private float[] mRotationMatrix = new float[16];
+public void onDrawFrame(GL10 gl) {
+    ...
+    float[] scratch = new float[16];
+
+    // Create a rotation transformation for the triangle
+    long time = SystemClock.uptimeMillis() % 4000L;
+    float angle = 0.090f * ((int) time);
+    Matrix.setRotateM(mRotationMatrix, 0, angle, 0, 0, -1.0f);
+
+    // Combine the rotation matrix with the projection and camera view
+    // Note that the mMVPMatrix factor *must be first* in order
+    // for the matrix multiplication product to be correct.
+    Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+
+    // Draw triangle
+    mTriangle.draw(scratch);
+}
