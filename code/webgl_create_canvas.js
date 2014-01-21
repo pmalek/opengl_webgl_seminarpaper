54b522ef943a544445e597a82964eb1ad056a55b
Patryk Malek <malekpatryk@gmail.com>
2014-01-21 02:50:41 +0100

    Added stuff in WebGL section.

--- code/webgl_create_canvas.js
+++ code/webgl_create_canvas.js
@@ -1,6 +1,11 @@
-// create Canvas element
+// create Canvas element dynamically
 var canvas = document.createElement("canvas");
 document.body.appendChild(canvas);
-// get the context
-var gl = canvas.getContext("experimental-webgl");
+// or if canvas element is already in the DOM one can get it by ID
+//var canvas = document.getElementById("canvas");
+
+var gl = null;
+// Try to grab the standard context. If it fails, fallback to experimental.
+gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+
 //we are ready to code with WebGL's context
--- code/webgl_square.js
+++ code/webgl_square.js
@@ -1,5 +1,10 @@
-gl.clearColor(0.0, 0.0, 0.0, 1.0);
-gl.enable(gl.DEPTH_TEST);
+if(gl) {
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);                 // Set clear color to black, fully opaque
+  gl.enable(gl.DEPTH_TEST);                          // Enable depth testing
+  gl.depthFunc(gl.LEQUAL);                           // Near things obscure far things
+  // Clear the color as well as the depth buffer.
+  gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT); 
+}
 
 var squareVertexPositionBuffer = gl.createBuffer();
 // binding "current array buffer" so that each following operation will be operated on it
